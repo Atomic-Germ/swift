5208744fb296ac6732f7618db7ec63e80a099fea
Casey J. Parker <02-germane-atomic@icloud.com>
2022-04-19 14:49:24 -0700

    modified:   Sources/DifferenceOfSquares/DifferenceOfSquares.swift 	added skeleton code for basic understanding

--- exercises/practice/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
+++ exercises/practice/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -1,1 +1,10 @@
-//Solution goes in Sources
+//
+// This is only a SKELETON file for the 'Difference Of Squares' exercise. 
+// It's been provided as a convenience to get you started writing code faster.
+//
+
+// Define a type `Squares` with members `squareOfSum`, `sumOfSquares`,
+// `differenceOfSquares`
+struct Squares {
+
+}
